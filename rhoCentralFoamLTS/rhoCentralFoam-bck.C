78617646ee5c5ae824064920778c3c3700195e6d
jiaqiwang969 <jiaqiwang969@gmail.com>
2022-06-17 02:23:28 +0800

    add LTS test

--- rhoCentralFoamLTS/rhoCentralFoam-bck.C
+++ rhoCentralFoamLTS/rhoCentralFoam-bck.C
@@ -2,8 +2,10 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+    \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2016 OpenFOAM Foundation
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -22,14 +24,14 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    rhoCentralRK4Foam
+    rhoCentralFoam
+
+Group
+    grpCompressibleSolvers
 
 Description
-    rhoCentralRK4Foam is derived from rhoCentralFoam by replacing its
-    first-order time advancement scheme with a third-order, four stage
-    Runge-Kutta scheme. The main purpose of developing this solver is to
-    improve the scaling performance of rhoCentralFoam, especially in
-    large-scale simulations
+    Density-based compressible flow solver based on central-upwind
+    schemes of Kurganov and Tadmor.
 
 \*---------------------------------------------------------------------------*/
 
@@ -41,10 +43,6 @@ Description
 #include "localEulerDdtScheme.H"
 #include "fvcSmooth.H"
 
-   bool adjustTimeStep = false;
-   scalar maxCo =0;
-   scalar maxDeltaT =0;
-
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 int main(int argc, char *argv[])
@@ -55,21 +53,16 @@ int main(int argc, char *argv[])
         " schemes of Kurganov and Tadmor."
     );
 
-
     #define NO_CONTROL
     #include "postProcess.H"
-   
+
     #include "addCheckCaseOptions.H"
     #include "setRootCaseLists.H"
     #include "createTime.H"
     #include "createMesh.H"
     #include "createFields.H"
     #include "createFieldRefs.H"
-    //#include "createTimeControls.H"
- 
-    #include "readThermophysicalProperties.H"
-    #include "readTimeControls.H"
-    #include "variables.H"
+    #include "createTimeControls.H"
 
     turbulence->validate();
 
@@ -77,47 +70,36 @@ int main(int argc, char *argv[])
 
     #include "readFluxScheme.H"
 
-    dimensionedScalar v_zero("v_zero", dimVolume/dimTime, 0.0);
+    const dimensionedScalar v_zero(dimVolume/dimTime, Zero);
 
     // Courant numbers used to adjust the time-step
     scalar CoNum = 0.0;
+    scalar meanCoNum = 0.0;
+
     Info<< "\nStarting time loop\n" << endl;
 
-    while (runTime.loop()) //Start time loop
-	//while (runTime.run())
-    {	
-		Info<< "Time = " << runTime.timeName() << nl << endl;
-	Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-            << "  Just enter while loop = " << runTime.elapsedClockTime() << " s"
-            << nl << endl;
-		//     Saving quantities at preavious time step
-       rhoOld  = rho;
-       rhoUOld = rhoU;
-       rhoEOld = rhoE;
-		//     RK Time step
-       for (int cycle =0; cycle < rkCoeff.size(); cycle++)
-       {
-
+    while (runTime.run())
+    {
         // --- Directed interpolation of primitive fields onto faces
-        //#include "interpolation.H"
 
-		surfaceScalarField rho_pos(interpolate(rho, pos));
+        surfaceScalarField rho_pos(interpolate(rho, pos));
         surfaceScalarField rho_neg(interpolate(rho, neg));
 
         surfaceVectorField rhoU_pos(interpolate(rhoU, pos, U.name()));
         surfaceVectorField rhoU_neg(interpolate(rhoU, neg, U.name()));
 
-        volScalarField rPsi("rPsi", 1.0/psi);//
-//        Info<< "max(rPsi)=" << max(mag(rPsi)) << nl << endl;
-//        Info<< "min(rPsi)=" << min((rPsi)) << nl << endl;
-	surfaceScalarField rPsi_pos(interpolate(rPsi, pos, T.name()));
-	surfaceScalarField rPsi_neg(interpolate(rPsi, neg, T.name()));
+        volScalarField rPsi("rPsi", 1.0/psi);
+        surfaceScalarField rPsi_pos(interpolate(rPsi, pos, T.name()));
+        surfaceScalarField rPsi_neg(interpolate(rPsi, neg, T.name()));
+
         surfaceScalarField e_pos(interpolate(e, pos, T.name()));
-	surfaceScalarField e_neg(interpolate(e, neg, T.name()));
+        surfaceScalarField e_neg(interpolate(e, neg, T.name()));
+
         surfaceVectorField U_pos("U_pos", rhoU_pos/rho_pos);
-	surfaceVectorField U_neg("U_neg", rhoU_neg/rho_neg);
+        surfaceVectorField U_neg("U_neg", rhoU_neg/rho_neg);
+
         surfaceScalarField p_pos("p_pos", rho_pos*rPsi_pos);
-	surfaceScalarField p_neg("p_neg", rho_neg*rPsi_neg);
+        surfaceScalarField p_neg("p_neg", rho_neg*rPsi_neg);
 
         surfaceScalarField phiv_pos("phiv_pos", U_pos & mesh.Sf());
         // Note: extracted out the orientation so becomes unoriented
@@ -125,25 +107,25 @@ int main(int argc, char *argv[])
         surfaceScalarField phiv_neg("phiv_neg", U_neg & mesh.Sf());
         phiv_neg.setOriented(false);
 
-
-
         volScalarField c("c", sqrt(thermo.Cp()/thermo.Cv()*rPsi));
-//        Info<< "max(c)=" << max(mag(c)) << nl << endl;
         surfaceScalarField cSf_pos
         (
             "cSf_pos",
             interpolate(c, pos, T.name())*mesh.magSf()
         );
+
         surfaceScalarField cSf_neg
         (
             "cSf_neg",
             interpolate(c, neg, T.name())*mesh.magSf()
         );
+
         surfaceScalarField ap
         (
             "ap",
             max(max(phiv_pos + cSf_pos, phiv_neg + cSf_neg), v_zero)
         );
+
         surfaceScalarField am
         (
             "am",
@@ -151,32 +133,53 @@ int main(int argc, char *argv[])
         );
 
         surfaceScalarField a_pos("a_pos", ap/(ap - am));
+
         surfaceScalarField amaxSf("amaxSf", max(mag(am), mag(ap)));
+
         surfaceScalarField aSf("aSf", am*a_pos);
+
         if (fluxScheme == "Tadmor")
         {
             aSf = -0.5*amaxSf;
             a_pos = 0.5;
         }
+
         surfaceScalarField a_neg("a_neg", 1.0 - a_pos);
+
         phiv_pos *= a_pos;
         phiv_neg *= a_neg;
+
         surfaceScalarField aphiv_pos("aphiv_pos", phiv_pos - aSf);
         surfaceScalarField aphiv_neg("aphiv_neg", phiv_neg + aSf);
+
         // Reuse amaxSf for the maximum positive and negative fluxes
         // estimated by the central scheme
         amaxSf = max(mag(aphiv_pos), mag(aphiv_neg));
-        //#include "readTimeControls.H"
-        c = Foam::sqrt(thermo.Cp()/thermo.Cv()/psi);
-		// construct Eulerian flux (convective + pressure fluxes)
+
+        #include "centralCourantNo.H"
+        #include "readTimeControls.H"
+
+        if (LTS)
+        {
+            #include "setRDeltaT.H"
+        }
+        else
+        {
+            #include "setDeltaT.H"
+        }
+
+        ++runTime;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
         phi = aphiv_pos*rho_pos + aphiv_neg*rho_neg;
+
         surfaceVectorField phiU(aphiv_pos*rhoU_pos + aphiv_neg*rhoU_neg);
         // Note: reassembled orientation from the pos and neg parts so becomes
         // oriented
         phiU.setOriented(true);
 
         surfaceVectorField phiUp(phiU + (a_pos*p_pos + a_neg*p_neg)*mesh.Sf());
-
 
         surfaceScalarField phiEp
         (
@@ -185,26 +188,33 @@ int main(int argc, char *argv[])
           + aphiv_neg*(rho_neg*(e_neg + 0.5*magSqr(U_neg)) + p_neg)
           + aSf*p_pos - aSf*p_neg
         );
-        volScalarField muEff("muEff", turbulence->muEff()); // turbulent viscosity
-        volTensorField tauMC("tauMC", muEff*dev2(Foam::T(fvc::grad(U)))); // viscous stress tensor
 
-		volScalarField rhoFl("rhoFl", fvc::div(phi));
-        volVectorField momFl("momFl", fvc::div(phiUp));
-		volVectorField momFl2("momFl2", fvc::div(tauMC));
-		volVectorField momFl3("momFl3", fvc::laplacian(muEff, U));
+        volScalarField muEff("muEff", turbulence->muEff());
+        volTensorField tauMC("tauMC", muEff*dev2(Foam::T(fvc::grad(U))));
+
         // --- Solve density
-		Info<< "solve rho" << nl << endl;
-        rho  = rhoOld + rkCoeff[cycle]*runTime.deltaT()*(
-                - rhoFl);
+        solve(fvm::ddt(rho) + fvc::div(phi));
+
         // --- Solve momentum
-		Info<< "solve rhoU" << nl << endl;
-		rhoU = rhoUOld + rkCoeff[cycle]*runTime.deltaT()*(
-                - momFl + momFl2 + momFl3);
+        solve(fvm::ddt(rhoU) + fvc::div(phiUp));
+
         U.ref() =
             rhoU()
            /rho();
         U.correctBoundaryConditions();
         rhoU.boundaryFieldRef() == rho.boundaryField()*U.boundaryField();
+
+        if (!inviscid)
+        {
+            solve
+            (
+                fvm::ddt(rho, U) - fvc::ddt(rho, U)
+              - fvm::laplacian(muEff, U)
+              - fvc::div(tauMC)
+            );
+            rhoU = rho*U;
+        }
+
         // --- Solve energy
         surfaceScalarField sigmaDotU
         (
@@ -215,20 +225,14 @@ int main(int argc, char *argv[])
             )
           & (a_pos*U_pos + a_neg*U_neg)
         );
-		volScalarField enFl("enFl", fvc::div(phiEp));
-		volScalarField enFl2("enFl2", fvc::div(sigmaDotU));
-		volScalarField enFl3("enFl3", fvc::laplacian(turbulence->alphaEff(), e));
-		volScalarField k("k", thermo.Cp()*muEff/Pr);//thermal diffusivity // correct
-		surfaceScalarField kave
+
+        solve
         (
-            fvc::interpolate(k)
+            fvm::ddt(rhoE)
+          + fvc::div(phiEp)
+          - fvc::div(sigmaDotU)
         );
-		surfaceScalarField heatFlux("heatFlux", kave*fvc::snGrad(T)*mesh.magSf());
-		
-		
-		Info<< "solve rhoE" << nl << endl;
-		rhoE = rhoEOld + rkCoeff[cycle]*runTime.deltaT()*(
-                -enFl + enFl2);
+
         e = rhoE/rho - 0.5*magSqr(U);
         e.correctBoundaryConditions();
         thermo.correct();
@@ -238,22 +242,28 @@ int main(int argc, char *argv[])
                 e.boundaryField() + 0.5*magSqr(U.boundaryField())
             );
 
-		Info<< "solve p" << nl << endl;
+        if (!inviscid)
+        {
+            solve
+            (
+                fvm::ddt(rho, e) - fvc::ddt(rho, e)
+              - fvm::laplacian(turbulence->alphaEff(), e)
+            );
+            thermo.correct();
+            rhoE = rho*(e + 0.5*magSqr(U));
+        }
+
         p.ref() =
             rho()
            /psi();
         p.correctBoundaryConditions();
         rho.boundaryFieldRef() == psi.boundaryField()*p.boundaryField();
-		
-        turbulence->correct();
-		}//end of RK time integration
 
-        #include "diagnostics.H" //print tke on diagnostics.dat
+        turbulence->correct();
+
         runTime.write();
-		//#include "step.H"      
-        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
-            << nl << endl;
+
+        runTime.printExecutionTime(Info);
     }
 
     Info<< "End\n" << endl;
